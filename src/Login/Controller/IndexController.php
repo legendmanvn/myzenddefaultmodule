fcc9c3c8ac80aacb9c7c431bb83bd6366ae874dd
legendmanvn <darkzmanvn@gmail.com>
2016-06-07 14:34:42 +0000

    default module

---
+++ src/Login/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Login\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController{
+	public function indexAction(){
+		return new ViewModel;
+	} 
+	public function testAction(){
+		return new ViewModel;
+	} 
+}
